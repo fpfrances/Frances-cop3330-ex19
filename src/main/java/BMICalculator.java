aeb7bf7828bd834e3084fb625f4cdc7e34c03b3e
fpfrances <fpfrances@hotmail.com>
2021-09-11 15:38:46 -0400

    Initial commit

---
+++ src/main/java/BMICalculator.java
@@ -0,0 +1,25 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Filipe Frances
+ */
+import java.util.Scanner;
+public class BMICalculator {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Height in Inches: ");
+        double height = sc.nextDouble();
+        System.out.print("Enter the weight in pounds: ");
+        double weight = sc.nextDouble();
+        double BMI = (weight / (height * height)) * 703;
+        System.out.println("Your BMI is " +String.format("%.2f",BMI));
+        if(BMI >= 18.5 && BMI <= 25){
+            System.out.println("You are within the ideal weight range");
+        }
+        else if(BMI > 25){
+            System.out.println("You are overweight. You should see your doctor.");
+        }
+        else if(BMI < 18.5){
+            System.out.println("You are underweight. You should consult with your doctor.");
+        }
+    }
+}
